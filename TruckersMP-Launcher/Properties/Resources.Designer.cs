19b9deedda9a7680d922e738476e823eca91d76d
Bluscream <admin@timo.de.vc>
2017-01-09 13:40:13 +0100

    added NAudio via NuGet

--- TruckersMP-Launcher/Properties/Resources.Designer.cs
+++ TruckersMP-Launcher/Properties/Resources.Designer.cs
@@ -151,16 +151,6 @@ namespace truckersmplauncher.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
-        /// </summary>
-        internal static byte[] NAudio {
-            get {
-                object obj = ResourceManager.GetObject("NAudio", resourceCulture);
-                return ((byte[])(obj));
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap play_mp_btn {
